5d1299269d1c888484b8215218d4d19c4909369c
Stefanny Merly Cancha Rodriguez <stefannycr1901@gmail.com>
2024-08-11 01:48:52 +0000

    dbt build -- select int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,12 @@
 select
-    products_id
-    ,(CAST(purchase_price as float64)*quantity) as purchase_cost
-    ,(revenue-(CAST(purchase_price as float64)*quantity))/revenue AS MARGIN
-FROM {{ref("stg_raw__sales")}}
-left join {{ref("stg_raw__product")}}
+    products_id,
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    CAST(purchase_price AS FLOAT64) as purchase_price,
+    ROUND(CAST(P.purchase_price as float64)*s.quantity,2) as purchase_cost,
+    round(s.revenue,2) - ROUND(CAST(P.purchase_price as float64)*s.quantity,2) as margin
+FROM {{ref("stg_raw__sales")}} s
+left join {{ref("stg_raw__product")}} p
 USING(products_id)
