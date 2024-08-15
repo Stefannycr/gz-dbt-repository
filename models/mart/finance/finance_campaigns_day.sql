96af872430560df2b344f8f634c592c63a99240c
Stefanny Cancha <stefannycr1901@gmail.com>
2024-08-15 23:54:36 +0000

    table finance campaing days

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,17 +1,17 @@
 select
     f.date_date,
     (operational_margin-ads_cost) as ads_margin,
-    average_basket
-    operational_margin,
-    ads_cost,
-    margin,
-    purchase_cost,
-    shipping_fee,
-    logcost,
-    ship_cost,
-    quantity, 
-    impression,
-    clicks,
+    f.average_basket,
+    f.operational_margin,
+    c.ads_cost,
+    f.margin,
+    f.purchase_cost,
+    f.shipping_fee,
+    f.logcost,
+    f.ship_cost,
+    f.quantity, 
+    c.impression,
+    c.clicks
     
 from {{ref("finance_days")}} f
 left join {{ref("int_campaigns_day")}} c
